a33e9d53b3b714233f76c7805c4385696c13e7bf
VDias88 <viniciusmonteirodias5@gmail.com>
2022-03-30 18:14:10 -0300

    Base das paginas

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className='homeGreatings'>
+        <div className="primaryBackground">
+
+        </div>
+        <div className="secundaryBackground">
+            
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home
